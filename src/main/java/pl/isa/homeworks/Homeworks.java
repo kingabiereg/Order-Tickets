118448d33383d6cec3227ffd06c5b933bdc15a16
Tomasz Odwald <tomasz.odwald@gmail.com>
2023-03-05 19:52:36 +0100

    Added GuessNumberApp for zadanie1

--- src/main/java/pl/isa/homeworks/Homeworks.java
+++ src/main/java/pl/isa/homeworks/Homeworks.java
@@ -1,3 +1,5 @@
+package pl.isa.homeworks;
+
 public class Homeworks {
     public static void main(String[] args) {
         System.out.println("Homeworks repository");
---
+++ src/main/java/pl/isa/homeworks/zadanie1/GuessNumberGame.java
@@ -0,0 +1,21 @@
+package pl.isa.homeworks.zadanie1;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+public class GuessNumberGame {
+    public String getIntro() {
+        throw new NotImplementedException("method getIntro() is not implemented");
+    }
+
+    public boolean isRunning() {
+        throw new NotImplementedException("method isRunning() is not implemented");
+    }
+
+    public String getInstruction() {
+        throw new NotImplementedException("method getInstruction() is not implemented");
+    }
+
+    public String guessNumber(int number) {
+        throw new NotImplementedException("method guessNumber() is not implemented");
+    }
+}
