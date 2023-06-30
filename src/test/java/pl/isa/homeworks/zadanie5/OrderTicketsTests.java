a4f65780b25c70e6eb6f5e5dc2fbf57a90c115e5
Kinga Biereg <stachewicz.k@gmail.com>
2023-06-30 22:48:11 +0200

    added test which check if total price is correct for many different tickets

--- src/test/java/pl/isa/homeworks/zadanie5/OrderTicketsTests.java
+++ src/test/java/pl/isa/homeworks/zadanie5/OrderTicketsTests.java
@@ -70,7 +70,35 @@ public class OrderTicketsTests {
         assertEquals(30, OrderTickets.takeOrder(people).getTotalPrice());
     }
 
+    @Test
+    @DisplayName("Should return total price when buy many different tickets")
+    void shouldReturnTotalPriceWhenBuyManyDifferentTickets() {
 
+        List<Person> people = new ArrayList<>();
+
+        Person person1 = new Person(1, false);
+        Person person2 = new Person(4, false);
+        Person person3 = new Person(7, false);
+        Person person4 = new Person(13, false);
+        Person person5 = new Person(16, true);
+        Person person6 = new Person(25, true);
+        Person person7 = new Person(20, false);
+        Person person8 = new Person(28, false);
+        Person person9 = new Person(99, false);
+
+        people.add(person1);
+        people.add(person2);
+        people.add(person3);
+        people.add(person4);
+        people.add(person5);
+        people.add(person6);
+        people.add(person7);
+        people.add(person8);
+        people.add(person9);
+
+
+        assertEquals(89, OrderTickets.takeOrder(people).getTotalPrice());
+    }
 
     @Test
     void getTotalPriceWhenPeopleAreLearning() {
